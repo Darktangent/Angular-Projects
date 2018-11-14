3469a791d7639f52a6e2c7ee301f18a4444d1e78
Rohan <c1ph3r9@gmail.com>
2018-11-14 10:08:41 -0600

    adding array implementaation

--- Data-Structure/hasPairWithSum.js
+++ Data-Structure/hasPairWithSum.js
@@ -1,15 +1,14 @@
 
-
 function pairWithSum(arr,sum){
 
   let j=1
   for (i=0; i<arr.length; i++){
-    if(arr[i]+arr[j]===sum){
-      return true
-    }else{
+    if(!(arr[i]+arr[j])===sum){
       j++
+      return false
+    }else{
+      return true
     }
   }
-  return false
-}
+  }
 pairWithSum([1,2,3,4,5],6)
