b673f7dd6df478286d1299095ad46afa8b7594ec
Rohan <c1ph3r9@gmail.com>
2018-11-15 08:00:49 -0600

    fixing find first duplicate function

--- Data-Structure/firstDuplicate.js
+++ Data-Structure/firstDuplicate.js
@@ -5,14 +5,16 @@ function firstDup(arr){
 
   let tail=arr.length
   let head=0
-  console.log(j)
-  for (head;head<arr.length-1;head++){
+  console.log(tail)
+  for (head;head<arr.length-1;){
 
     if(arr[head]!==arr[tail]){
       console.log('inside',arr[tail])
       tail--
     }else if (arr[head]===arr[tail]){
       return (`Found it: ${arr[head]}`)
+    }else{
+      head++
     }
     
   }
@@ -21,4 +23,4 @@ return undefined
 
 
 }
-firstDup([22,34,22,1])
+firstDup([22,34,34,22,1])
