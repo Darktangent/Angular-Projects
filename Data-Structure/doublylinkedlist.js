cac7c75132b96f08ea44d599a329af0ca25248d7
Rohan <c1ph3r9@gmail.com>
2018-11-16 03:50:33 -0600

    add doubly linked list

--- Data-Structure/doublylinkedlist.js
+++ Data-Structure/doublylinkedlist.js
@@ -1,10 +1,11 @@
 // add a method remove() to the linked list that deletes a node to the specified index.
 
-class LinkedList {
+class DoublyLinkedList {
   constructor(value) {
     this.head = {
       value: value,
-      next: null
+      next: null,
+      prev:null
     };
     this.tail = this.head;
     this.length = 1;
@@ -12,8 +13,10 @@ class LinkedList {
   append(value) {
     const newNode = {
       value: value,
-      next: null
+      next: null,
+      prev:null
     }
+    newNode.prev=this.tail
     this.tail.next = newNode;
     this.tail = newNode;
     this.length++;
@@ -22,9 +25,12 @@ class LinkedList {
   prepend(value) {
     const newNode = {
       value: value,
-      next: null
+      next: null,
+      prev:null
     }
+
     newNode.next = this.head;
+    this.head.prev=newNode
     this.head = newNode;
     this.length++;
     return this;
@@ -47,12 +53,16 @@ class LinkedList {
     
     const newNode = {
       value: value,
-      next: null
+      next: null,
+      prev:null
     }
     const leader = this.traverseToIndex(index-1);
-    const holdingPointer = leader.next;
+    const follower = leader.next;
     leader.next = newNode;
-    newNode.next = holdingPointer;
+    newNode.prev=leader;
+    newNode.next = follower;
+    follower.prev=newNode;
+    
     this.length++;
     return this.printList();
   }
@@ -74,30 +84,12 @@ class LinkedList {
     this.length--;
     return this.printList();
   }
-  reverse(){
-    if(!this.head.next){
-      return this.head
-    }
-    let first=this.head
-    this.tail=this.head
-    let second=first.next
-    while(second){
-      const temp=second.next
-      second.next= first
-      first=second
-      second=temp
-    }
-    this.head.next=null
-    this.head=first
-return this.printList()
-
-
-  }
 }
 
-let myLinkedList = new LinkedList(10);
+let myLinkedList = new DoublyLinkedList(10);
 myLinkedList.append(5);
-myLinkedList.append(16);myLinkedList.prepend(1);
-myLinkedList.insert(2, 99);
-myLinkedList.insert(20, 88);
-myLinkedList.remove(2);
+myLinkedList.append(16);
+// myLinkedList.prepend(1);
+// myLinkedList.insert(2, 99);
+// myLinkedList.insert(20, 88);
+// myLinkedList.remove(2);
